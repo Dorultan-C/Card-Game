a20c5301c5b38622b172e2d497742d184a1acb29
Cristian Dorultan <c.dorultan@gmail.com>
2022-10-28 18:57:31 +0100

    Clean up function ready

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 import Deck from "./deck.js";
 
 const computerCardSlot = document.querySelector(".computer-card-slot")
+const playerCardSlot = document.querySelector(".player-card-slot")
+const computerDeckElement = document.querySelector('.computer-deck')
+const playerDeckElement = document.querySelector('.player-deck')
+const text = document.querySelector('.text')
 
-const deck = new Deck()
-deck.shuffle()
-computerCardSlot.appendChild(deck.cards[0].getHTML())
+let playerDeck, computerDeck 
+
+startGame()
+function startGame() {
+  const deck = new Deck()
+  deck.shuffle()
+
+  const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
+  playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
+  computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
+
+  console.log(playerDeck)
+  console.log(computerDeck)
+
+  cleanBeforeRound()
+}
+
+function cleanBeforeRound() {
+  computerCardSlot.innerHTML = ''
+  playerCardSlot.innerHTML = ''
+  text.innerText = '' 
+
+  updateDeckCount()
+}
+
+function updateDeckCount() {
+  computerDeckElement.innerText = computerDeck.numberOfCards
+  playerDeckElement.innerText = playerDeck.numberOfCards
+}
